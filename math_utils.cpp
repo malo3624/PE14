cf5bcb26e2f49d664a93d02b52d67625ef11ac97
malo3624 <malo3624@colorado.edu>
2018-04-10 17:35:42 -0600

    added IsSquare

--- math_utils.cpp
+++ math_utils.cpp
@@ -1,10 +1,15 @@
 // partners: Victoria Velasquez, Matthew Louchart
 
 #include <math_utils.h>
+#include <math.h>
 
 bool Math::IsSquare(int num)
 {
-  
+  int x = sqrt(num);
+  if(x == static_cast<int>(x))
+    return true;
+  else
+    return false;
 }
 
 int Math::GetDigit(int num, int place)
