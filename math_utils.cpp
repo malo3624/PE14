e2042064865b5d5d07d3ce631b67a73ccc33071f
malo3624 <malo3624@colorado.edu>
2018-04-10 17:23:12 -0600

    added funcs to cpp file

--- math_utils.cpp
+++ math_utils.cpp
@@ -1,3 +1,18 @@
 // partners: Victoria Velasquez, Matthew Louchart
 
 #include <math_utils.h>
+
+bool Math::IsSquare(int num)
+{
+  
+}
+
+int Math::GetDigit(int num, int place)
+{
+  
+}
+
+bool Math::EqualParity(int x, int y)
+{
+  
+}
