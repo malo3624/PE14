d23060e53e29f9e4d23512bc3a3ca7cb0a9841c8
malo3624 <malo3624@colorado.edu>
2018-04-15 16:09:58 -0600

    Update math_utils.cpp
    Quadratic fun added

--- math_utils.cpp
+++ math_utils.cpp
@@ -45,7 +45,20 @@ bool Math::EqualParity(std::vector nums)
     return false;
 }
 
-int Math::Quadratic(int x, int a, int b, int c)
+int Math::Quadratic(int x1, int x2, int a, int b, int c)
 {
-    
+    inSqrt = b*b - 4*a*c
+    if (inSqrt > 0){   
+        x1 = (-b + sqrt(b*b - 4*a*c) / (2*a);
+        x2 = (-b - sqrt(b*b - 4*a*c) / (2*a);
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+              }
+    else if (inSqrt == 0) {
+        x1 = (-b + sqrt(b*b - 4*a*c) / (2*a);
+        cout << "x1 and x2 = " << x1 << endl;
+              }
+    else {
+        cout << "cannot take square root of zero" << endl;
+    }
 }
