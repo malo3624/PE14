daae81b56b595015ff69c450a933ce71dfe06e2d
Victoria Velasquez <vive1564@colorado.edu>
2018-04-15 21:02:42 -0600

    Update math_utils.h

--- math_utils.h
+++ math_utils.h
@@ -2,13 +2,17 @@
 
 #ifndef _MATH_UTILS_
 #define _MATH_UTILS_
+#include <math.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 class Math{
 public:
     bool IsSquare(int num);
     int GetDigit(int num, int place);
     bool EqualParity(int x, int y);
-    bool EqualParity(std::vector nums)
+    bool EqualParity(std::vector nums);
     int Quadratic(int x, int a, int b, int c);
 };
 #endif  // _MATH_UTILS_
